2e8476f06253b59bd8b0946291f1923e28f04777
Carlos <carlos.av.amorim@gmail.com>
2022-08-10 20:38:35 -0300

    Style: Add swiper

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
 
           <meta name="UiQ Chick!" content="Chick é ser você!" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,80 @@
+import React, { useRef, useState } from "react";
 import type { GetStaticProps } from 'next'
 import { getPrismicClient } from '../services/prismic';
 
-interface Post {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+
+interface Slide {
   uid?: string;
   first_publication_date: string | null;
   data: {
     title: string;
-    author: string;
+    banner: {
+      alt: string;
+      url: string;
+    }
   };
 }
 
-interface PostPagination {
-  results: Post[];
+interface Slider {
+  results: Slide[];
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
-  postsPagination2: PostPagination;
+  sliderResults: Slider;
 }
 
-export default function Home({ postsPagination, postsPagination2 }: HomeProps) {
+export default function Home({ sliderResults }: HomeProps) {
   return (
     <>
-      <ul>{postsPagination?.results.map((item) => (
-        <li key={item.uid}>{item.data.title}</li>
-      ))}</ul>
-      <ul>{postsPagination2?.results.map((item) => (
-        <li key={item.uid}>{item.data.title}</li>
-      ))}</ul>
+
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        {/*  {
+          sliderResults?.results.map((item) => (
+            <SwiperSlide>
+              <img src={item.data.banner.url} alt={item.data.banner.alt} />
+            </SwiperSlide>
+          ))
+        } */}
+
+      </Swiper>
+
+      {/*  <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+        {
+          sliderResults?.results.map((item) => (
+            <SwiperSlide>
+              <img src={item.data.banner.url} alt={item.data.banner.alt} />
+            </SwiperSlide>
+          ))
+        }
+
+      </Swiper> */}
+
+
     </>
+
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
-  const postsResponse = await prismic.getByType(process.env.PRISMIC_API_CATEGORY_1 as string, {
+
+  /* const postsResponse = await prismic.getByType(process.env.PRISMIC_API_CATEGORY_1 as string, {
     orderings: {
       field: 'last_publication_date',
       direction: 'desc',
@@ -42,25 +82,24 @@ export const getStaticProps: GetStaticProps = async () => {
   });
 
   const postsPagination = {
-    results: postsResponse.results,
-  };
-
-  const postsResponse2 = await prismic.getByType(process.env.PRISMIC_API_CATEGORY_2 as string, {
+    results: postsResponse.results, */
+  const slider = await prismic.getByType(process.env.PRISMIC_API_SLIDER as string, {
     orderings: {
       field: 'last_publication_date',
       direction: 'desc',
     },
   });
 
-  const postsPagination2 = {
-    results: postsResponse2.results,
+  const sliderResults = {
+    results: slider.results,
   };
 
 
   return {
     props: {
-      postsPagination,
-      postsPagination2
+      sliderResults
     },
   };
-}
+
+};
+
